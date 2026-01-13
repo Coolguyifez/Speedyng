b5c97c18e37c535e76df1a62ca2694aead88c976
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-01-13 14:25:26 +0100

    Update AdminPanel.jsx

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -36,7 +36,7 @@ const AdminPanel = () => {
     year: new Date().getFullYear(),
     mileage: '',
     transmission: 'Automatic',
-    fuel_type: 'Petrol'
+    fuel_type: 'Petrol',
     description: '',
     features: ''
   });

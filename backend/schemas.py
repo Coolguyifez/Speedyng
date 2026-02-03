b461911327e6ffcfc502e5a8a6849094cc71df78
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-02-03 17:58:14 +0100

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -40,6 +40,9 @@ class CarBase(BaseModel):
     price: int
     condition: str
     location: str
+    address: Optional[str] = None       
+    phone_number: Optional[str] = None
+    owner_name: Optional[str] = None
     image: Optional[str] = None
     images: Optional[List[str]] = []
     year: Optional[int] = None
@@ -59,6 +62,9 @@ class CarUpdate(BaseModel):
     price: Optional[int]
     condition: Optional[str]
     location: Optional[str]
+    address: Optional[str]      
+    phone_number: Optional[str]
+    owner_name: Optional[str]
     image: Optional[str]
     images: Optional[List[str]]
     year: Optional[int]

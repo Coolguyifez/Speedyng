edfccd614d076300318474f149302313f0a53780
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-02-03 13:29:29 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # 1. Ensure the protocol is exactly this for Render
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    ""
+    "postgresql+asyncpg://speedy_db_ilfm_user:8OMvGcK6pLYYrfal1PUhIB0YczEIBhUz@dpg-d60ui3fgi27c73aujjn0-a.oregon-postgres.render.com/speedy_db_ilfm"
 )
 
 # 2. Async engine

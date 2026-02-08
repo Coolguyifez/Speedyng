bea2fd85e3c2081d024d6e7cb9895343bac8f82b
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-02-09 00:12:36 +0100

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
                 { label: 'Browse Vans', to: '/vehicles?type=Van' },
                 { label: 'Browse Buses', to: '/vehicles?type=Bus' },
                 { label: 'Browse Motocycles', to: '/vehicles?type=Motorcycle' },
-                { label: 'Browse Tricycles', to: '/vehicles?type=Tricycles' },
+                { label: 'Browse Tricycles', to: '/vehicles?type=Tricycle' },
                 { label: 'Contact Us', to: '/contact' }
               ].map((link) => (
                 <li key={link.label}>

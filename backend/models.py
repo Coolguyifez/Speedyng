0a0e6f389816b92812ee4ea554a823e564c11781
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-02-26 22:12:44 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -12,7 +12,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, nullable=False, index=True)
-    phone = Column(String(20), nullable=False)
+    phone = Column(String(20), nullable=True)
     password = Column(String(255), nullable=False)
     role = Column(String(20), default="user", nullable=False)
     favorites = Column(ARRAY(Integer), default=[])  # stores car IDs

da64198d31d85e44f92fad8f6267ee76381710af
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-03-01 09:47:58 +0100

    Update RegisterPage.jsx

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -139,8 +139,8 @@ const RegisterPage = () => {
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-200"></div>
             </div>
-            <div className="relative flex justify-center text-xs">
-              <span className="px-2 bg-white text-gray-500 font-medium">Or</span>
+            <div className="relative flex justify-center text-sm">
+              <span className="px-4 bg-white text-gray-500 font-medium">Or</span>
             </div>
           </div>
 

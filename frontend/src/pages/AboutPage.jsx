2aa9c55736bf0b87b55db031fe07bd39d815e98f
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-03-04 09:27:02 +0100

    Update AboutPage.jsx

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { ShieldCheck, Users, Zap, CheckCircle, Car, MessageSquare } from 'lucide-react';
+import { ShieldCheck, TrendingUp, Users, Zap, CheckCircle, Car, MessageSquare } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const AboutPage = () => {

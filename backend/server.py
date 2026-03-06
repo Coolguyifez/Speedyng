5613d4f295556f2d52cee3b38a5728b0637d9d58
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-03-06 06:28:27 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -512,8 +512,7 @@ async def create_vehicle(
     
 @api_router.put("/vehicles/{v_id}", response_model=VehicleResponse)
 async def update_vehicle(
-    async def update_vehicle(
-    vehicle_id: int,
+    v_id: int,
     name: Optional[str] = Form(None),
     type: Optional[str] = Form(None),
     price: Optional[str] = Form(None),
